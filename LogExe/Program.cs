f33ff59721e27360c1b2cdf4e8bcda91049a532d
enriquelr1195 <enrique_1195@hotmail.com>
2024-07-23 13:58:56 -0600

    Primer commit

---
+++ LogExe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csv = new StringBuilder();
+            var text = new StringBuilder();
+            DateTime datetime = DateTime.Now;
+            string filePath = "C:\\LogsData\\" + DateTime.Now.ToString("yyyy_MM") + "\\LogExe.csv";
+
+            //in your loop
+            int lenght = args.Length;
+            for (int i = 0; i < lenght; i++)
+            {
+                csv.Append(args[i] + ",");
+            }
+            csv.AppendLine("");
+
+            //after your loop
+            File.AppendAllText(filePath, csv.ToString());
+        }
+    }
+}
